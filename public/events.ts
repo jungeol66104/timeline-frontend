106a7b157f3cc9c3ccfc45567619fe2c1aa27e5c
Nam Joon <jungeol66104@gmail.com>
2023-09-22 20:40:39 +0900

    timeline with no contents complete

---
+++ public/events.ts
@@ -0,0 +1,39 @@
+import exp from "constants";
+
+const events = [
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    },
+    {
+        id: 0,
+        title: 'dummy'
+    }
+]
+
+export default events
+
+export interface Event {
+    id: number
+    title: string
+}
