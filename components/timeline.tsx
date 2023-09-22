17492068433fe7bb875425b2c0edf3b3a4fab92d
Nam Joon <jungeol66104@gmail.com>
2023-09-22 19:49:45 +0900

    timeline body and one eventbox

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 
 const Timeline = () => {
+    const numOfEvents: number = 7
+
     return (
-        <div className='m-5 relative'>
+        <div className='m-5'>
             <BodyLine />
             <EventBox />
         </div>
@@ -13,9 +15,10 @@ export default Timeline
 
 const BodyLine = () => {
     return (
-        <>
-            <canvas className="absolute"/>
-        </>
+        <div className={`w-3 h-5 relative`}>
+            <div className={`absolute w-0.5 h-[894px] bg-gray-400 left-1/2 `} style={{transform:'translate(-50%,-0)'}}></div>
+            <div className={`absolute w-0.5 h-[854px] bg-gray-600 left-1/2 top-5`} style={{transform:'translate(-50%,-0)'}}></div>
+        </div>
     )
 }
 
@@ -31,8 +34,8 @@ const EventBox = () => {
 
 const EventNode = () => {
     return (
-        <div>
-
+        <div className='w-3 mr-2.5 z-10'>
+            <div className='w-3 h-3 bg-white mr-2.5 border-2 rounded-full border-gray-600'></div>
         </div>
     )
 }
