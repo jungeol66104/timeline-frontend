5234dcc19591d554428397eb1aeea52c96f47ee0
Nam Joon <jungeol66104@gmail.com>
2023-11-07 14:52:54 +0900

    check viewportheight change

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ import SearchSVG from "../public/svg/search.svg"
 import Search from "./search"
 import { updateIsSearch } from "@/store/slices/searchSlice";
 import {selectCurrentEvents, selectCurrentTimeline} from "@/store/slices/contentsSlice";
-import {updateViewportHeight} from "@/store/slices/appearanceSlice";
+import {selectViewportHeight, updateViewportHeight} from "@/store/slices/appearanceSlice";
 // refactoring: needed
 
 const Layout = ({ children } : {children: ReactNode}) => {
@@ -44,11 +44,13 @@ export default Layout
 
 const Navbar = () => {
     const dispatch = useDispatch()
+    const viewportHeight = useSelector(selectViewportHeight)
 
     return (
         <div className={'fixed top-0 left-0 h-[60px] w-full bg-white pr-5 pl-5 shadow-md flex items-center justify-between z-30'}>
             <Link href={'/'} className={`font-black text-2xl transform transition-opacity ease-in-out duration-300`}>Timeline</Link>
             <div className={'flex items-center gap-2.5'}>
+                <div className={'absolute bottom-10'}>{viewportHeight}</div>
                 <button><Image src={SearchSVG} alt={'search'} width={24} height={24} onClick={() => dispatch(updateIsSearch())}/></button>
                 <button className={'hidden'}><Image src={MenuSVG} alt={'menu'} width={24} height={24} /></button>
             </div>
