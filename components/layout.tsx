a79b619c73a008fc3f0c147aabcd9a1f0274dde6
Nam Joon <jungeol66104@gmail.com>
2023-11-07 17:10:09 +0900

    temporary searchTest incarnated, need to discuss about good search feature

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,15 @@
-import {ReactNode, useEffect} from "react";
+import React, {ReactNode, useEffect} from "react";
 import Image from "next/image";
 import Link from 'next/link'
 import { useDispatch, useSelector } from "react-redux";
 import MenuSVG from "../public/svg/menu.svg"
 import SearchSVG from "../public/svg/search.svg"
 import Search from "./search"
-import { updateIsSearch } from "@/store/slices/searchSlice";
+import {selectIsSearch, updateIsSearch} from "@/store/slices/searchSlice";
 import {selectCurrentEvents, selectCurrentTimeline} from "@/store/slices/contentsSlice";
 import {selectViewportHeight, updateViewportHeight} from "@/store/slices/appearanceSlice";
 import SearchTest from "@/components/searchTest";
+import CloseSVG from "@/public/svg/close.svg";
 // refactoring: needed
 
 const Layout = ({ children } : {children: ReactNode}) => {
@@ -46,13 +47,15 @@ export default Layout
 
 const Navbar = () => {
     const dispatch = useDispatch()
-    const viewportHeight = useSelector(selectViewportHeight)
+    const isSearch = useSelector(selectIsSearch)
 
     return (
         <div className={'fixed top-0 left-0 h-[60px] w-full bg-white pr-5 pl-5 shadow-md flex items-center justify-between z-30'}>
             <Link href={'/'} className={`font-black text-2xl transform transition-opacity ease-in-out duration-300`}>Timeline</Link>
             <div className={'flex items-center gap-2.5'}>
-                <button><Image src={SearchSVG} alt={'search'} width={24} height={24} onClick={() => dispatch(updateIsSearch())}/></button>
+                <button onClick={() => dispatch(updateIsSearch())}>
+                    {!isSearch ? <Image src={SearchSVG} alt={'search'} width={24} height={24} /> : <Image src={CloseSVG} alt={'close'} width={24} height={24} />}
+                </button>
                 <button className={'hidden'}><Image src={MenuSVG} alt={'menu'} width={24} height={24} /></button>
             </div>
         </div>
--- components/searchTest.tsx
+++ components/searchTest.tsx
@@ -1,14 +1,81 @@
 import {useDispatch, useSelector} from "react-redux";
-import {selectIsSearch, updateIsSearch} from "@/store/slices/searchSlice";
+import {
+    selectIsSearch, selectSearchedEvents, selectSearchedTimelines,
+    selectSearchValue,
+    selectTab, updateIsSearch, updateSearchedEvents,
+    updateSearchedTimelines,
+    updateSearchValue,
+    updateTab
+} from "@/store/slices/searchSlice";
 import {selectViewportHeight} from "@/store/slices/appearanceSlice";
-import {RefObject, useEffect, useRef} from "react";
+import React, {RefObject, useEffect, useRef} from "react";
+import api from "@/utils/api";
+import Image from "next/image";
+import CloseSVG from "@/public/svg/close.svg";
+import SearchInBarSVG from "@/public/svg/searchInBar.svg";
+import {TimelineEvent} from "@/public/events";
+import Link from "next/link";
+import NorthwestSVG from "@/public/svg/northwest.svg";
 
 const SearchTest = () => {
+    const isSearch = useSelector(selectIsSearch)
+
+    return (
+        <>
+            {isSearch ? <SearchContents/> : <></>}
+        </>
+    )
+}
+export default SearchTest
+
+const SearchContents = () => {
+    const viewportHeight = useSelector(selectViewportHeight)
+
+    return (
+        <div className={'fixed top-[60px] pt-2.5 left-0 w-screen bg-white'} style={{height: viewportHeight - 60, zIndex: 20}}>
+            <SearchHeader />
+            <SearchBody />
+        </div>
+    )
+}
+
+
+const SearchHeader = () => {
     const searchBarInputRef: RefObject<HTMLInputElement> = useRef(null)
 
     const dispatch = useDispatch()
-    const viewportHeight = useSelector(selectViewportHeight)
     const isSearch = useSelector(selectIsSearch)
+    const searchValue = useSelector(selectSearchValue)
+    const tab = useSelector(selectTab)
+
+    const handelSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value
+        dispatch(updateSearchValue(query))
+
+        const fetchSearchResults = async (query: any) => {
+            if (query === '') return { timelines: [], events: [] }
+            try {
+                const timelineResponse = await api.post('/v1/search', {"searchType": "timeline", "text": query})
+                const eventResponse = await api.post('/v1/search', {"searchType": "event", "text": query})
+                const timelines = timelineResponse.data.data.searchResult
+                const events = eventResponse.data.data.searchResult
+                return { timelines, events }
+            } catch (error) {
+                console.error('Error fetching searched timelines and events: ', error);
+                return { timelines: [], events: [] }
+            }
+        }
+        const operateSearch = async () => {
+            try {
+                const { timelines, events } = await fetchSearchResults(query)
+                dispatch(updateSearchedTimelines(timelines))
+                dispatch(updateSearchedEvents(events))
+            } catch (error) {
+                console.error('Error updating timelines, events and query: ', error);
+            }
+        }
+        operateSearch()
+    }
 
     useEffect(() => {
         const searchBarInput = searchBarInputRef.current
@@ -17,11 +84,62 @@ const SearchTest = () => {
             searchBarInput.focus()
     }, [isSearch]);
 
-    let searchHeight = viewportHeight === 0 ? 'calc(-100vh)': viewportHeight - 60
     return (
-        <div onClick={() => dispatch(updateIsSearch())} className={'fixed flex items-center justify-center bg-gray-600 w-screen'} style={{zIndex: 9999, height: searchHeight, top: !isSearch ? -searchHeight : 60, transition: 'all 0.3s'}}>
-            <input ref={searchBarInputRef}/>
+        <div>
+            <div className={'h-[50px] flex gap-2.5 ml-4 mr-4 pt-2.5 pb-2.5 border-b-[1px]'}>
+                <div className={'flex-shrink-0 w-7 h-7 bg-gray-500 rounded-full flex align-middle justify-center'}><Image src={SearchInBarSVG} alt={'searchInBar'} width={16} height={16} /></div>
+                <input ref={searchBarInputRef} onChange={handelSearch} value={searchValue} placeholder={'Search...'} className={'w-full focus:outline-0'}></input>
+            </div>
+            <SearchTab />
         </div>
     )
 }
-export default SearchTest
+
+const SearchTab = () => {
+    const dispatch = useDispatch()
+    const tab = useSelector(selectTab)
+
+    return (
+        <div className={'h-fit flex flex-col ml-4 mr-4'}>
+            <div className={'flex'}>
+                <div onClick={() => dispatch(updateTab('timeline'))} className={`cursor-pointer pt-2.5 pb-2.5 w-1/2 text-center transition-colors duration-300 ease-in-out ${tab === 'timeline' ? 'text-gray-600' : 'text-gray-400' } font-semibold text-[14px]`}>타임라인</div>
+                <div onClick={() => dispatch(updateTab('event'))} className={`cursor-pointer pt-2.5 pb-2.5 w-1/2 text-center transition-colors duration-300 ease-in-out ${tab === 'event' ? 'text-gray-600' : 'text-gray-400' }  font-semibold text-[14px]`}>이벤트</div>
+            </div>
+            <div className={`w-1/2 border-b-2 border-gray-500 transform transition-transform ease-in-out duration-300 ${tab === 'timeline' ? '-translate-x-0' : 'translate-x-full'}`}></div>
+        </div>
+    )
+}
+
+const SearchBody = () => {
+
+    const tab = useSelector(selectTab)
+    const searchedTimelines = useSelector(selectSearchedTimelines)
+    const searchedEvents = useSelector(selectSearchedEvents)
+
+    return (
+        <div className={`flex w-fit transform transition-transform ease-in-out duration-300 ${tab === 'timeline' ? 'translate-x-0' : '-translate-x-1/2'}`}>
+            <div className={'page w-screen overflow-scroll'} style={{height: `calc(98vh - 143px)`}}>
+                {searchedTimelines.map((timeline, i) => {
+                    return <SearchResultBox timeline={timeline} key={i}/>
+                })}
+            </div>
+            <div className={'page w-screen overflow-scroll'} style={{height: `calc(98vh - 143px)`}}>
+                {searchedEvents.map((event, i) => {
+                    return <SearchResultBox event={event} key={i}/>
+                })}
+            </div>
+        </div>
+    )
+}
+
+const SearchResultBox = ({timeline, event}: {timeline?: any,event?: TimelineEvent}) => {
+    return (
+        <Link href={ timeline ? `/timelines/${timeline.id}` : `/events/${event?.id}`}>
+            <div className={'flex items-center pt-[12px] pb-[12px] gap-2.5'}>
+                <div><Image src={NorthwestSVG} alt={'northwest'} width={20} height={20} /></div>
+                <div className={'font-black'}>{event ? event.name : timeline.name }</div>
+                <div className={'text-sm text-gray-500'}>{event ? event.date :`#`}</div>
+            </div>
+        </Link>
+    )
+}
