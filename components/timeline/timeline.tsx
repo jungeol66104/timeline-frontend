fd8eb4fac47bf5a2067eb792e9c38afd920301cc
Nam Joon <jungeol66104@gmail.com>
2023-11-08 10:25:35 +0900

    colors to indentify divs

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -293,7 +293,7 @@ const Timeline = () => {
         };
     });
     return (
-        <div className='timeline flex flex-col max-w-lg relative'>
+        <div className='timeline bg-blue-500 flex flex-col max-w-lg relative'>
             <TimelineFrame />
             <TimelineEvents />
             {(lastAction === 'zoom') && <AfterEffectEvents />}
