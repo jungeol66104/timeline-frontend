0b9d98c6ad10b0a52261d3a0b1dc6b41ac8b47d7
Nam Joon <jungeol66104@gmail.com>
2023-11-08 13:47:09 +0900

    same

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {storeWrapper} from '@/store/store'
-import {Provider, useDispatch} from "react-redux";
+import {Provider} from "react-redux";
 import Layout from '../components/layout'
-import {useEffect} from "react";
-import {updateViewportHeight} from "@/store/slices/appearanceSlice";
 // refactoring; clear
 
 function App({ Component, ...rest }: AppProps) {
