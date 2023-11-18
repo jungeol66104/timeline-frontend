ed4cd006a630a893263955f5c51b90920ab9ac0c
Nam Joon <jungeol66104@gmail.com>
2023-11-18 18:08:24 +0900

    before fixing cross browser click and touch mechanism

--- components/timeline/eventContent.tsx
+++ components/timeline/eventContent.tsx
@@ -40,7 +40,7 @@ const EventContent = ({event, highestEvent, contentOrder, isToggle} : {event: Ti
         const operateToggle = async (e: MouseEvent) => {
             try {
                 if (!isToggle && contentOrder === 0 && event.overlap !== 0) {
-                    // e.preventDefault()
+                    e.preventDefault()
                     let { newToggleEvents, newTotalHeight} = await fetchToggleEvents()
                     dispatch(updateToggleEvents({order: eventOrderInCurrent, toggleEvents: newToggleEvents}))
                     dispatch(updateIsToggle(eventOrderInCurrent))
