891c70704f07e2973c4ea8db2b89a00012fa9d73
Nam Joon <jungeol66104@gmail.com>
2023-11-21 00:09:39 +0900

    timeline zindex 9998

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -306,7 +306,7 @@ const Timeline = () => {
     });
 
     return (
-        <div className='timeline absolute w-full overflow-hidden' style={{height: totalHeight + 140, zIndex: 0}}>
+        <div className='timeline absolute w-full overflow-hidden' style={{height: totalHeight + 140, zIndex: 9998}}>
             <TimelineFrame />
             <Link href={'/'} className={'relative'} style={{zIndex: 9999}}>timeline</Link>
             <TimelineEvents />
