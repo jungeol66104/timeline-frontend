1f97b4956e6a6caba2071e6324312eab5b4b01d6
Nam Joon <jungeol66104@gmail.com>
2023-12-28 16:03:06 +0900

    tool bar test

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/layout/navbar";
 import {useSelector} from "react-redux";
 import {selectIsSearch} from "@/store/slices/searchSlice";
 import {useRouter} from "next/router";
+import Toolbar from "@/components/layout/toolbar";
 // refactoring: clear
 
 const Layout = ({ children } : {children: ReactNode}) => {
@@ -16,6 +17,7 @@ const Layout = ({ children } : {children: ReactNode}) => {
             <Navbar />
             <>{children}</>
             { (isHome || isSearch) ? <Search /> : <></> }
+            {<Toolbar />}
         </div>
     )
 }
---
+++ components/layout/toolbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from "next/image";
+import HorizontalSplitSVG from '@/public/svg/horizontalSplit.svg'
+import LastPageSVG from '@/public/svg/lastPage.svg'
+import FirstPageSVG from '@/public/svg/firstPage.svg'
+import NavigateBeforeSVG from '@/public/svg/NavigateBefore.svg'
+import NavigateNextSVG from '@/public/svg/NavigateNext.svg'
+
+const Toolbar = () => {
+    return (
+        <div className={'fixed bottom-[20px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[190px] h-[40px] border-[1px] rounded-3xl bg-white drop-shadow-md'} style={{zIndex: 4999}}>
+            <div className={'flex items-center gap-[10px]'}>
+                <button><Image src={FirstPageSVG} alt={'last depth'} /></button>
+                <button><Image src={NavigateBeforeSVG} alt={'plus one depth'} /></button>
+                <button><Image src={HorizontalSplitSVG} alt={'timeline menu'} /></button>
+                <button><Image src={NavigateNextSVG} alt={'minus one depth'} /></button>
+                <button><Image src={LastPageSVG} alt={'first depth'} /></button>
+            </div>
+        </div>
+    );
+};
+
+export default Toolbar;
