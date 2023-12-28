302e6412bed400a714aa1452ef5e3ea37b085377
Nam Joon <jungeol66104@gmail.com>
2023-12-28 16:23:32 +0900

    return to white bg tool bar

--- components/layout/toolbar.tsx
+++ components/layout/toolbar.tsx
@@ -8,13 +8,13 @@ import NavigateNextSVG from '@/public/svg/NavigateNext.svg'
 
 const Toolbar = () => {
     return (
-        <div className={'fixed bottom-[20px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[190px] h-[40px] border-[1px] rounded-3xl bg-black drop-shadow-md'} style={{zIndex: 4999}}>
-            <div className={'flex items-center gap-[10px]'}>
-                <button><Image src={FirstPageSVG} alt={'last depth'} /></button>
-                <button><Image src={NavigateBeforeSVG} alt={'plus one depth'} /></button>
-                <button><Image src={HorizontalSplitSVG} alt={'timeline menu'} /></button>
-                <button><Image src={NavigateNextSVG} alt={'minus one depth'} /></button>
-                <button><Image src={LastPageSVG} alt={'first depth'} /></button>
+        <div className={'fixed bottom-[20px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[200px] h-[45px] border-[1px] rounded-3xl bg-white drop-shadow-md'} style={{zIndex: 4999}}>
+            <div className={'flex items-center'}>
+                <button className={'px-[6px]'}><Image src={FirstPageSVG} alt={'last depth'} /></button>
+                <button className={'px-[6px]'}><Image src={NavigateBeforeSVG} alt={'plus one depth'} /></button>
+                <button className={'px-[7px] border-x-[1px]'}><Image src={HorizontalSplitSVG} alt={'timeline menu'} /></button>
+                <button className={'px-[6px]'}><Image src={NavigateNextSVG} alt={'minus one depth'} /></button>
+                <button className={'px-[6px]'}><Image src={LastPageSVG} alt={'first depth'} /></button>
             </div>
         </div>
     );
