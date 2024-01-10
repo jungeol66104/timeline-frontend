2fa9c26a95ad75d687dd670c5d9569a21c2ac095
Nam Joon <jungeol66104@gmail.com>
2024-01-10 09:05:23 +0900

    dynamic head, sitemap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import {Provider} from "react-redux";
 import type { AppProps } from 'next/app'
 import {storeWrapper} from '@/store/store'
 import Layout from '@/components/layout/layout'
-import DynamicHead from "@/components/dynamicHead";
 // refactoring; clear
 
 function App({ Component, ...rest }: AppProps) {
     const {store, props} = storeWrapper.useWrappedStore(rest)
     return (
         <Provider store={store}>
-            <DynamicHead type={'root'} />
             <Layout>
                   <Component {...props.pageProps} />
             </Layout>
