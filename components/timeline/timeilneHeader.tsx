90be39b6230cdc0751fb613ab670bedfe0de9df1
Nam Joon <jungeol66104@gmail.com>
2024-01-10 16:26:18 +0900

    timeline header image

--- components/timeline/timeilneHeader.tsx
+++ components/timeline/timeilneHeader.tsx
@@ -7,8 +7,8 @@ const TimelineHeader = () => {
     const currentTimeline = useSelector(selectCurrentTimeline)
 
     return (
-        <div className={'absolute left-[-10px] flex gap-2.5'}>
-            <div className={'w-[32px] h-[32px]'}><Image className={'rounded-sm'} src={`/images/timeline/${currentTimeline.id}.png`} alt={`${currentTimeline.name}`} width={32} height={32} /></div>
+        <div className={'absolute left-[-6px] flex items-center gap-2.5'}>
+            <div className={'w-[24px] h-[24px] mb-[1px]'}><Image className={'rounded-sm'} src={`/images/timeline/${currentTimeline.id}.png`} alt={`${currentTimeline.name}`} width={32} height={32} /></div>
             <div className={'text-2xl font-semibold'}>{currentTimeline.name}</div>
         </div>
     );
