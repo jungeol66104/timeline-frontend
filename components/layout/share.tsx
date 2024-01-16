88853bfe0cb35400b43de41b6544eb1bb4132fb7
Nam Joon <jungeol66104@gmail.com>
2024-01-17 08:54:48 +0900

    share btn

--- components/layout/share.tsx
+++ components/layout/share.tsx
@@ -14,26 +14,26 @@ const Share = () => {
 
     const bottom = isShare ? 0 : -170
     return (
-        <div className={'share fixed px-5 w-full max-w-lg h-[170px] left-1/2 transform -translate-x-1/2 bg-white rounded-t-2xl flex flex-col items-center'} style={{zIndex:5002, bottom: bottom,  transition: 'bottom 0.3s'}}>
-            <h2 className={'font-semibold text-md py-2.5'}>공유</h2>
-            <div className={'w-full border-y-[1px] flex gap-[30px] py-5'}>
-                <div className={'flex flex-col gap-2.5 items-center'}>
+        <div className={'share fixed w-full max-w-lg h-[170px] left-1/2 transform -translate-x-1/2 bg-white rounded-t-2xl flex flex-col items-center'} style={{zIndex:5002, bottom: bottom,  transition: 'bottom 0.3s'}}>
+            <h2 className={'font-semibold text-md py-2.5 border-b-[1px] w-full text-center'}>공유</h2>
+            <div className={'w-full px-5 pb-2.5 flex gap-[30px] py-5 overflow-x-auto'}>
+                <div className={'flex flex-col w-fit shrink-0  gap-2.5 items-center'}>
                     <button className={'w-[40px] h-[40px] border-[1px] border-gray-600 rounded-full flex justify-center items-center'}><Image src={LinkSVG} alt={'link copy'} width={24} height={24} /></button>
                     <div className={'text-xs'}>링크 복사</div>
                 </div>
-                <div className={'flex flex-col gap-2.5 items-center'}>
+                <div className={'flex flex-col w-fit shrink-0  gap-2.5 items-center'}>
                     <button className={'w-[40px] h-[40px] border-[1px] border-gray-600 rounded-full flex justify-center items-center'}><Image src={KakaotalkSVG} alt={'kakaotalk'} width={24} height={24} /></button>
                     <div className={'text-xs'}>카카오톡</div>
                 </div>
-                <div className={'flex flex-col gap-2.5 items-center'}>
+                <div className={'flex flex-col w-fit shrink-0  gap-2.5 items-center'}>
                     <button className={'w-[40px] h-[40px] border-[1px] border-gray-600 rounded-full flex justify-center items-center'}><Image src={XSVG} alt={'x'} width={18} height={18} /></button>
                     <div className={'text-xs'}>X</div>
                 </div>
-                <div className={'flex flex-col gap-2.5 items-center'}>
+                <div className={'flex flex-col w-fit shrink-0 gap-2.5 items-center'}>
                     <button className={'w-[40px] h-[40px] border-[1px] border-gray-600 rounded-full flex justify-center items-center'}><Image src={FacebookSVG} alt={'facebook'} width={24} height={24} /></button>
                     <div className={'text-xs'}>페이스북</div>
                 </div>
-                <div className={'flex flex-col gap-2.5 items-center'}>
+                <div className={'flex flex-col w-fit shrink-0 gap-2.5 items-center'}>
                     <button className={'w-[40px] h-[40px] border-[1px] border-gray-600 rounded-full flex justify-center items-center'}><Image src={MoreSVG} alt={'more'} width={24} height={24} /></button>
                     <div className={'text-xs'}>더보기</div>
                 </div>
--- components/timeline/timeilneHeader.tsx
+++ components/timeline/timeilneHeader.tsx
@@ -4,6 +4,7 @@ import {selectCurrentTimeline} from "@/store/slices/contentsSlice";
 import Image from "next/image";
 import ShareSVG from "@/public/svg/share.svg"
 import {updateIsShare} from "@/store/slices/appearanceSlice";
+import Link from "next/link";
 
 const TimelineHeader = () => {
     const dispatch = useDispatch()
@@ -14,16 +15,20 @@ const TimelineHeader = () => {
     }
 
     return (
-        <>
-            <div className={'text-2xl font-semibold'}>{currentTimeline.name}</div>
-            <div className={'w-[24px] h-[24px] top-0 right-0 mb-[0.5px]'}><Image className={'rounded-sm'} src={`/images/timeline/${currentTimeline.id}.png`} alt={`${currentTimeline.name}`} width={28} height={28} /></div>
+        <div className={'relative w-fit flex gap-2.5 items-center'}>
+            <Link onClick={() => sessionStorage.clear()} href={`/timelines/${currentTimeline.id}`} className={`relative w-fit font-black text-2xl`}>
+                <div className={'flex gap-2.5 items-center'}>
+                    <div className={'text-2xl font-semibold'}>{currentTimeline.name}</div>
+                    <div className={'w-[24px] h-[24px] top-0 right-0 mb-[0.5px]'}><Image className={'rounded-sm'} src={`/images/timeline/${currentTimeline.id}.png`} alt={`${currentTimeline.name}`} width={28} height={28} /></div>
+                </div>
+            </Link>
             <div onClick={handleClick} className={'cursor-pointer flex items-center pr-[6px] h-[24px] mb-[0.5px] rounded-sm bg-white border-[0.1px] shadow-[0_2px_3px_rgba(0,0,0,0.07)]'}>
                 <div className={'flex w-[24px] h-[24px] items-center justify-center'}>
                     <Image src={ShareSVG} alt={'share'} width={14} height={14}/>
                 </div>
                 <div className={'text-xs font-semibold'}>공유</div>
             </div>
-        </>
+        </div>
     );
 };
 
