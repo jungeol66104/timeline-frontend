b271c0202647466c379a298e6a61b344c158eea3
Nam Joon <jungeol66104@gmail.com>
2024-01-17 08:34:07 +0900

    layout for share

---
+++ components/layout/overlay.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {selectIsShare, updateIsShare} from "@/store/slices/appearanceSlice";
+
+const Overlay = () => {
+    const dispatch = useDispatch()
+    const isShare = useSelector(selectIsShare)
+
+
+    const handleClick = () => {
+        dispatch(updateIsShare())
+    }
+
+    return (
+        <div onClick={handleClick} className={`${isShare ? 'opacity-10' : 'pointer-events-none opacity-0'} absolute w-full max-w-lg h-full top-0 left-1/2 transform -translate-x-1/2 bg-black`} style={{zIndex: 5001, transition: 'opacity 0.3s'}}></div>
+    );
+};
+
+export default Overlay;
--- components/timeline/timeilneHeader.tsx
+++ components/timeline/timeilneHeader.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectCurrentTimeline} from "@/store/slices/contentsSlice";
 import Image from "next/image";
 import ShareSVG from "@/public/svg/share.svg"
+import {updateIsShare} from "@/store/slices/appearanceSlice";
 
 const TimelineHeader = () => {
+    const dispatch = useDispatch()
     const currentTimeline = useSelector(selectCurrentTimeline)
+
+    const handleClick = () => {
+        dispatch(updateIsShare())
+    }
 
     return (
         <>
             <div className={'text-2xl font-semibold'}>{currentTimeline.name}</div>
             <div className={'w-[24px] h-[24px] top-0 right-0 mb-[0.5px]'}><Image className={'rounded-sm'} src={`/images/timeline/${currentTimeline.id}.png`} alt={`${currentTimeline.name}`} width={28} height={28} /></div>
-            <div className={'cursor-pointer flex items-center pr-[6px] h-[24px] mb-[0.5px] rounded-sm bg-white border-[0.1px] shadow-[0_2px_3px_rgba(0,0,0,0.07)]'}>
+            <div onClick={handleClick} className={'cursor-pointer flex items-center pr-[6px] h-[24px] mb-[0.5px] rounded-sm bg-white border-[0.1px] shadow-[0_2px_3px_rgba(0,0,0,0.07)]'}>
                 <div className={'flex w-[24px] h-[24px] items-center justify-center'}>
                     <Image src={ShareSVG} alt={'share'} width={14} height={14}/>
                 </div>
