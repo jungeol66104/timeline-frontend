b9be4dad31106afeea3064d394d5940e29c85f0a
Nam Joon <jungeol66104@gmail.com>
2024-01-19 16:18:15 +0900

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import {GA_TRACKING_ID} from "@/utils/gtags";
 // refactoring: needed (look around if there is anything that I can implement)
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                        page: window.location.pathname
+                    });`
+              }}
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ utils/gtags.ts
@@ -0,0 +1,38 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export const GA_TRACKING_ID = 'G-CKYR6VZ0BJ';
+
+export const pageview = (url: URL) => {
+    window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+    });
+};
+
+export const event = (
+    action: Gtag.EventNames,
+    { event_category, event_label, value }: Gtag.EventParams,
+) => {
+    window.gtag('event', action, {
+        event_category,
+        event_label,
+        value,
+    });
+};
+
+export const useGtag = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'development') return;
+
+        const handleRouteChange = (url: URL) => {
+            pageview(url);
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+};
