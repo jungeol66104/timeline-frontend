7f84c21658711f95ee507595150e03e4e1525454
Nam Joon <jungeol66104@gmail.com>
2024-01-22 06:17:19 +0900

    size

--- components/event/relatedTimeline.tsx
+++ components/event/relatedTimeline.tsx
@@ -12,11 +12,11 @@ const RelatedTimeline = () => {
         <div className={'flex flex-col gap-2.5'}>
             <h2 className={'text-lg font-semibold'}>연관된 타임라인</h2>
             {currentEvent.timelineInfo?.map((tI, i) => {
-                    return (<Link key={i} href={`/timelines/${tI.id}`} className={'h-[60px] bg-white shadow-md rounded-lg border-[1px] flex gap-2.5 items-center px-2.5'} style={{width: 'calc((100% - 20px)/2.15)'}}>
-                                <div className={'shrink-0'}><Image src={`/images/timeline/${tI.id}.png`} alt={tI.name} width={40} height={40} className={'rounded-md'}/></div>
+                    return (<Link key={i} href={`/timelines/${tI.id}`} className={'h-[65px] w-[230px] bg-white shadow-md rounded-lg border-[1px] flex gap-2.5 items-center px-2.5'}>
+                                <div className={'shrink-0'}><Image src={`/images/timeline/${tI.id}.png`} alt={tI.name} width={45} height={45} className={'rounded-md'}/></div>
                                 <div>
-                                    <div className={'font-semibold text-sm line-clamp-1'}>{tI.name}</div>
-                                    <div className={'text-xs line-clamp-1 opacity-90'}>{(temporarySeries[2]["timelines"] as any[]).find(timeline => timeline.id === tI.id).description}</div>
+                                    <div className={'font-semibold text-md line-clamp-1'}>{tI.name}</div>
+                                    <div className={'text-sm line-clamp-1 opacity-90'}>{(temporarySeries[2]["timelines"] as any[]).find(timeline => timeline.id === tI.id).description}</div>
                                 </div>
                             </Link>)
                 })
