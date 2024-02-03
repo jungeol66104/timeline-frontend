d45162c77c0853301ab9594c12bf3f85a84e0692
Nam Joon <jungeol66104@gmail.com>
2024-02-03 14:25:43 +0900

    search debounce

--- components/layout/search/searchHeader.tsx
+++ components/layout/search/searchHeader.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import SearchInBarSVG from "@/public/svg/searchInBar.svg";
 import SearchTab from "@/components/layout/search/searchTab";
 import {useRouter} from "next/router";
+import useDebounce from "@/hooks/useDebounce";
 // refactoring: clear
 
 const SearchHeader = () => {
@@ -16,16 +17,12 @@ const SearchHeader = () => {
     const dispatch = useDispatch()
     const isSearch = useSelector(selectIsSearch)
     const searchValue = useSelector(selectSearchValue)
+    const debouncedSearchValue = useDebounce(searchValue, 100)
 
-    const handelSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        const query = e.target.value
+    useEffect(() => {
+        const query = debouncedSearchValue
 
         const fetchSearchResults = async (query: any) => {
-            // initial timelines and events must be fetched
-            if (query === '') {
-                const initialTimelines: any = []
-                return { timelines: initialTimelines, events: [] }
-            }
             try {
                 const eventResponse = await api.get(`/search/event?searchText=${query}`, {headers: {lang: 'en'}})
                 const timelineResponse = await api.get(`/search/timeline?searchText=${query}`, {headers: {lang: 'en'}})
@@ -37,6 +34,7 @@ const SearchHeader = () => {
                 return { timelines: [], events: [] }
             }
         }
+
         const operateSearch = async () => {
             try {
                 const { timelines, events } = await fetchSearchResults(query)
@@ -46,9 +44,10 @@ const SearchHeader = () => {
                 console.error('Error updating timelines, events and query: ', error);
             }
         }
-        await operateSearch()
-        dispatch(updateSearchValue(query))
-    }
+
+        operateSearch()
+    }, [debouncedSearchValue]);
+
 
     // when search feature activates, focus to the searchbar in order to write query immediately
     useEffect(() => {
@@ -62,7 +61,7 @@ const SearchHeader = () => {
         <div>
             <div className={'h-[50px] flex gap-2.5 py-2.5 border-b-[1px]'}>
                 <div className={'flex-shrink-0 w-7 h-7 bg-gray-500 rounded-full flex align-middle justify-center'}><Image src={SearchInBarSVG} alt={'searchInBar'} width={16} height={16} /></div>
-                <input ref={searchBarInputRef} onChange={handelSearch} value={searchValue} placeholder={'Search...'} className={'w-full'} style={{outline: 'none'}}></input>
+                <input ref={searchBarInputRef} onChange={(e) => dispatch(updateSearchValue(e.target.value))} value={searchValue} placeholder={'Search...'} className={'w-full'} style={{outline: 'none'}}></input>
             </div>
             <SearchTab />
         </div>
