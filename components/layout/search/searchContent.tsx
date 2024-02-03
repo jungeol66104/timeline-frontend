a2f66a3d88863a3d3597926534ab4b6910cb68d8
Nam Joon <jungeol66104@gmail.com>
2024-02-03 09:03:15 +0900

    temporary images

--- components/layout/search/searchContent.tsx
+++ components/layout/search/searchContent.tsx
@@ -13,9 +13,14 @@ const SearchContent = ({searchResult}: {searchResult: any}) => {
     return (
         <Link href={ tab === 'timeline' ? `/timelines/${searchResult.id}` : `/events/${searchResult.id}`}>
             <div className={'flex items-center pt-[6px] pb-[6px] gap-2.5'}>
-                {tab === 'timeline' && <div className={'searchImage w-[28px] h-[28px] relative'}><Image className={'rounded-sm'} src={`/images/timeline/small/${searchResult.id}.png`} alt={`${searchResult.name}`} width={28} height={28} priority={true}/></div>}
+                {tab === 'timeline' && <div className={'searchImage w-[28px] h-[28px] relative'}>
+                    {searchResult.id <= 10
+                        ?   <Image className={'rounded-sm'} src={`/images/timeline/small/${searchResult.id}.png`} alt={`${searchResult.name}`} width={28} height={28} priority={true}/>
+                        :   <div className={'w-full h-full rounded-sm bg-gray-500 text-white flex items-center justify-center text-sm'}><span>{searchResult.name.charAt(0).toUpperCase()}</span></div>
+                    }
+                </div>}
                 <div className={'font-medium flex-1 line-clamp-1'} >{searchResult.name}</div>
-                <div className={'text-sm text-gray-500'}>{tab === 'timeline' ? "" : searchResult.date}</div>
+                <div className={' text-sm text-gray-500'}>{tab === 'timeline' ? "" : searchResult.date}</div>
             </div>
         </Link>
     )
