8e6859ad30abe1830ee05809c7e85c67abedfb15
Nam Joon <jungeol66104@gmail.com>
2024-02-21 15:40:30 +0900

    total height and absoulte and useEffect scrolltop depend

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -3,14 +3,18 @@ import TimelineFrame from "@/components/timeline/timelineFrame";
 import TimelineEvents from "@/components/timeline/timelineEvents";
 import useOperateTimeline from "@/hooks/useOperateTimeline";
 import useScrollSetup from "@/hooks/useScrollSetup";
+import {useSelector} from "react-redux";
+import {selectTotalHeight} from "@/store/slices/appearanceSlice";
 // refactoring: clear
 
 const Timeline = () => {
+    const totalHeight = useSelector(selectTotalHeight)
+
     useScrollSetup()
     useOperateTimeline()
 
     return (
-        <div className='timeline relative h-fit w-full max-w-[670px]'>
+        <div className='timeline absolute h-fit w-full max-w-[670px]' style={{height: totalHeight + 140}}>
             <TimelineFrame />
             <TimelineEvents />
         </div>
