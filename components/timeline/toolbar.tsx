e19ad4b63d9b49a6eb6b0d297d32bd770229c54b
Nam Joon <jungeol66104@gmail.com>
2024-03-13 19:07:01 +0900

    toolbar issmall

--- components/timeline/toolbar.tsx
+++ components/timeline/toolbar.tsx
@@ -9,6 +9,7 @@ import {current} from "immer";
 const Toolbar = () => {
     const maxDepth = useSelector(selectMaxDepth)
     const currentDepth = useSelector(selectCurrentDepth)
+    const isSmallToolbar = maxDepth === 0
 
     useLayoutEffect(() => {
         const toolbar : HTMLDivElement | null = typeof window !== 'undefined' ? document.querySelector('.toolbar') : null
@@ -21,16 +22,25 @@ const Toolbar = () => {
 
     return (
         <div className={`fixed bottom-[18px] w-full max-w-[684px]`} style={{zIndex: 4998}}>
-            <div className={'toolbar absolute bottom-0 flex border-[1px] rounded-lg bg-white/50 drop-shadow-md h-[40px]'} style={{right: 32}}>
-                { currentDepth === maxDepth
-                    ?   <button className={'toolbarButton summary flex items-center justify-center text-sm font-medium w-[80px]'}>Summary</button>
-                    :   <button className={'toolbarButton showAll flex items-center justify-center text-sm font-medium w-[80px]'}>Show All</button>
-                }
-                <div className={'border-r-[1px]'}></div>
-                <button className={'toolbarButton uppermost flex items-center justify-center w-[40px]'}>
-                    <div><Image src={NorthSVG} alt={'uppermost'} height={20} width={20}/></div>
-                </button>
-            </div>
+            {isSmallToolbar
+                ? <div className={'toolbar absolute bottom-0 flex border-[1px] rounded-lg bg-white/50 drop-shadow-md h-[40px]'} style={{right: 32}}>
+                    <button className={'toolbarButton uppermost flex items-center justify-center w-[40px]'}>
+                        <div><Image src={NorthSVG} alt={'uppermost'} height={20} width={20}/></div>
+                    </button>
+                </div>
+                : <div
+                    className={'toolbar absolute bottom-0 flex border-[1px] rounded-lg bg-white/50 drop-shadow-md h-[40px]'}
+                    style={{right: 32}}>
+                    {currentDepth === maxDepth
+                        ?   <button className={'toolbarButton summary flex items-center justify-center text-sm font-medium w-[80px]'}>Summary</button>
+                            :   <button className={'toolbarButton showAll flex items-center justify-center text-sm font-medium w-[80px]'}>Show All</button>
+                        }
+                        <div className={'border-r-[1px]'}></div>
+                        <button className={'toolbarButton uppermost flex items-center justify-center w-[40px]'}>
+                            <div><Image src={NorthSVG} alt={'uppermost'} height={20} width={20}/></div>
+                        </button>
+                    </div>
+            }
         </div>
     );
 };
