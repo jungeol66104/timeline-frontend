55a7135431d714e0503e4f2b16d8a4a355eb0fc9
Nam Joon <jungeol66104@gmail.com>
2024-03-20 17:52:58 +0900

    sitemap and robots.txt

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -5,15 +5,16 @@ import api from "@/utils/api";
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     if (res) {
         const timelineResponse = await api.get('/timeline', {headers: {lang: 'en'}})
-        const eventResponse = await api.get('/event', {headers: {lang: 'en'}})
         const timelines: any[] = timelineResponse.data.data
+        const timelineIds = timelines.map(timeline => timeline.id)
+        const urls = [...timelineIds.map(timelineId => `https://timeline.vg/timelines/${timelineId}`)]
+
+        // const eventResponse = await api.get('/event', {headers: {lang: 'en'}})
         // const timelines: any[] = timelineResponse.data.data.slice(0,10)
         // const events: any[] = eventResponse.data.data
         // const events: any[] = eventResponse.data.data.slice(0,10)
-        const timelineIds = timelines.map(timeline => timeline.id)
         // const eventIds = events.map(event => event.id)
         // const urls = [...timelineIds.map(timelineId => `https://timeline.vg/timelines/${timelineId}`), ...eventIds.map(eventId => `https://timeline.vg/events/${eventId}`)]
-        const urls = [...timelineIds.map(timelineId => `https://timeline.vg/timelines/${timelineId}`)]
 
         res.setHeader('Content-Type', 'text/xml')
         res.write(`<?xml version="1.0" encoding="UTF-8"?>
