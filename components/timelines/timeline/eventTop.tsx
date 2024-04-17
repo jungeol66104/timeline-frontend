b9006df09b27f8e65d37f1c6846663c602a96bbc
Nam Joon <jungeol66104@gmail.com>
2024-04-17 09:18:35 +0900

    zindex

--- components/timelines/timeline/eventTop.tsx
+++ components/timelines/timeline/eventTop.tsx
@@ -4,7 +4,7 @@ const EventTop = ({isEnd} : {isEnd: boolean}) => {
     return (
         <div className={'timelineEventsTop flex items-center w-full'}>
             {isEnd
-                ?   <div className={'h-fit w-full bg-white'} style={{zIndex: 5000}}>
+                ?   <div className={'h-fit w-full bg-white'} style={{zIndex: 30}}>
                         <h3 className={'text-[20px] py-3 font-bold'}>Timeline</h3>
                     </div>
                 :   <div className={'h-[60px]'}></div>
