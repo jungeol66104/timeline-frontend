98c5ebc7f14fd139ba5824260ecbb76db05de7e2
Nam Joon <jungeol66104@gmail.com>
2024-04-18 14:33:36 +0900

    hotjar test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,12 +17,22 @@ export default function Document() {
                   __html: `window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${GA_TRACKING_ID}', {
-                        page: window.location.pathname
-                    });`
+                    gtag('config', '${GA_TRACKING_ID}', {page: window.location.pathname});`
               }}
           />
-          <HotjarScript />
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `(function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:4947576,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+              }}
+          />
+          {/*<HotjarScript />*/}
       </Head>
       <body>
         <Main />
