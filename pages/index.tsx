31984ab1772f2cc5f7b65ac136c876c94e158dca
Nam Joon <jungeol66104@gmail.com>
2024-04-24 22:03:09 +0900

    test hr

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                                         </div>
                                     </div>
                                 </div>
-                                <hr/>
+                                <hr className={'border-gray-200'}/>
                             </Link>
                         )
                     })}
