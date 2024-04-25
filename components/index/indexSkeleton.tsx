a58dfbd746c92ba1012da66e3a34f6f3eb4f9d74
Nam Joon <jungeol66104@gmail.com>
2024-04-25 13:46:23 +0900

    tagbar skel

--- components/index/indexSkeleton.tsx
+++ components/index/indexSkeleton.tsx
@@ -4,8 +4,16 @@ const IndexSkeleton = () => {
     const skeletonArray = Array(10).fill(0)
 
     return (
-        <div className={'page indexPage pt-[44px]'}>
-            <div className={'animate-pulse relative h-fit w-full max-w-[600px] px-4 pt-1 pb-0'}>
+        <div className={'page indexPage animate-pulse'}>
+            <div className={`flex pt-2 pb-1.5 h-fit w-full border-b-[1px] border-gray-100 bg-white overflow-x-auto`}>
+                <div className={'w-full flex gap-2 px-4'}>
+                    {/*<Link href={'/'} className={`h-[32px] w-fit px-3 flex items-center justify-center rounded-3xl border-[1px] ${currentTag === 'Hot' ? 'border-black' : 'border-gray-200 hover:bg-gray-100'} bg-white text-sm font-semibold shrink-0`}><span>&#x1F525; Hot</span></Link>*/}
+                    {/*<Link href={'/'} className={`h-[32px] w-fit px-3 flex items-center justify-center rounded-3xl border-[1px] ${currentTag === 'Staff Picks' ? 'border-black' : 'border-gray-200 hover:bg-gray-100'} bg-white text-sm font-semibold shrink-0`}><span>Staff Picks</span></Link>*/}
+                    <div className={`h-[32px] w-fit px-3 flex items-center justify-center rounded-3xl border-[1px] bg-gray-100 shrink-0`}><span className={'text-gray-100'}>Popular</span></div>
+                    <div className={`h-[32px] w-fit px-3 flex items-center justify-center rounded-3xl border-[1px] bg-gray-100 shrink-0`}><span className={'text-gray-100'}>Recently Added</span></div>
+                </div>
+            </div>
+            <div className={'relative h-fit w-full max-w-[600px] px-4 pt-1 pb-0'}>
                 {skeletonArray.map((_, i) => {
                     return (
                         <div key={i} className={'h-[132.67px]'}>
@@ -19,9 +27,15 @@ const IndexSkeleton = () => {
                                             <div className={'w-full h-4 bg-gray-100 rounded-md'} style={{width: '60%'}}></div>
                                         </div>
                                         <p className={'mt-1 w-full flex flex-col'}>
-                                            <div className={'w-full h-5'}><div className={'w-full h-4 bg-gray-100'}></div></div>
-                                            <div className={'w-full h-5'}><div className={'w-full h-4 bg-gray-100'}></div></div>
-                                            <div className={'w-full h-5'}><div className={'h-4 bg-gray-100'} style={{width: '80%'}}></div></div>
+                                            <div className={'w-full h-5'}>
+                                                <div className={'w-full h-4 bg-gray-100'}></div>
+                                            </div>
+                                            <div className={'w-full h-5'}>
+                                                <div className={'w-full h-4 bg-gray-100'}></div>
+                                            </div>
+                                            <div className={'w-full h-5'}>
+                                                <div className={'h-4 bg-gray-100'} style={{width: '80%'}}></div>
+                                            </div>
                                         </p>
                                     </div>
                                     <div className={'relative w-[84px] h-[84px] shrink-0'}>
