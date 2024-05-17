322315e21fd28b5e7257fc94edab24816bad009a
Nam Joon <jungeol66104@gmail.com>
2024-05-18 07:24:30 +0900

    bar fixed

--- components/layout/informationBar.tsx
+++ components/layout/informationBar.tsx
@@ -6,7 +6,7 @@ const InformationBar = () => {
     useInformationBarTest()
 
     return (
-        <div className={'informationHeader hidden sticky top-[60px] pt-[5px] pb-[3px] w-full flex-col bg-white border-b-[1px]'} style={{zIndex: 4999}}>
+        <div className={'informationHeader hidden fixed top-[60px] pt-[5px] pb-[3px] w-full flex-col bg-white border-b-[1px]'} style={{zIndex: 4999}}>
             <span className={'informationHeaderName pl-4 w-full max-w-[630px] min-[852px]:max-w-[1002px] text-sm font-semibold'}></span>
         </div>
     );
