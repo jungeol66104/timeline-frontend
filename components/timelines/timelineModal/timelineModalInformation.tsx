c410f7f04aed5b964d2251efdaf6c34967f8be8a
Nam Joon <jungeol66104@gmail.com>
2024-05-30 19:33:32 +0900

    height

--- components/timelines/timelineModal/timelineModalInformation.tsx
+++ components/timelines/timelineModal/timelineModalInformation.tsx
@@ -23,7 +23,7 @@ const TimelineModalInformation = () => {
 
     const bottom = timelineModalType === 'information' ? 0 : '-100%'
     return (
-        <div className={'timelineModalInformation fixed w-full max-w-lg h-full left-1/2 transform -translate-x-1/2 bg-white rounded-t-2xl flex flex-col items-center'} style={{zIndex: 5002, height: 'calc(100% - 90px)', bottom: bottom, transition: 'bottom 0.3s'}}>
+        <div className={'timelineModalInformation fixed w-full max-w-lg h-full left-1/2 transform -translate-x-1/2 bg-white rounded-t-2xl flex flex-col items-center'} style={{zIndex: 5002, height: 'calc(100% - 61px)', bottom: bottom, transition: 'bottom 0.3s'}}>
             <div className={'relative py-2.5 border-b-[1px] w-full text-center'}>
                 <h2 className={'font-semibold text-md'}>Information</h2>
                 <button className={'absolute right-4 top-3 w-[20px] h-[20px] shrink-0'}><Image onClick={handleClick} src={'/svg/close.svg'} alt={'close'} width={20} height={20}/></button>
