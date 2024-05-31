58b8c15f3efbd662b6a0dc2c36d596cdf5d9f897
Nam Joon <jungeol66104@gmail.com>
2024-05-31 20:43:22 +0900

    black font

--- components/timelines/timelineModal/saveButton.tsx
+++ components/timelines/timelineModal/saveButton.tsx
@@ -12,7 +12,7 @@ const SaveButton = () => {
 
     return (
         <button onClick={handleSave} className={`px-3 flex items-center justify-center gap-1.5 h-[36px] border-[0.1px] border-gray-300 bg-black drop-shadow-sm rounded-md`}>
-            <div className={'text-sm font-semibold text-white'}>Save</div>
+            <div className={'text-sm font-medium text-white'}>Save</div>
         </button>
     )
 };
