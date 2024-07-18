31725b628ebd911497fd169f4b75ecf74f794510
Nam Joon <jungeol66104@gmail.com>
2024-07-18 16:08:50 +0900

    m test

---
+++ components/modal/informationModal/informationDescriptionEdit.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import {EditorContent, useEditor} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+import {selectCurrentTimelineDraft, updateCurrentTimelineDraft} from "@/store/slices/contentsSlice";
+import {useDispatch, useSelector} from "react-redux";
+
+const InformationDescriptionEdit = () => {
+    const dispatch = useDispatch()
+    const currentTimelineDraft = useSelector(selectCurrentTimelineDraft)
+
+    const editor = useEditor({
+        extensions: [StarterKit, Image],
+        editorProps: {
+            attributes: {class: 'outline-none'}
+        },
+        onUpdate: ({ editor }) => {
+            dispatch(updateCurrentTimelineDraft({...currentTimelineDraft, description: editor.getText()}))
+        },
+        content: `<p>${currentTimelineDraft.description}</p>`,
+    })
+
+    useEffect(() => {
+        if (!editor) return
+
+        editor.commands.setContent(`<p>${currentTimelineDraft.description}</p>`)
+    }, [currentTimelineDraft])
+
+    return (
+        <div className={'absolute'}><EditorContent editor={editor}/></div>
+    );
+};
+
+export default InformationDescriptionEdit;
--- components/modal/informationModal/informationModalHead.tsx
+++ components/modal/informationModal/informationModalHead.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import ModalMenubar from "@/components/modal/modalMenubar";
 import {useSelector} from "react-redux";
 import {selectCurrentTimeline} from "@/store/slices/contentsSlice";
+import {selectModalContentType} from "@/store/slices/appearanceSlice";
+import InformationDescriptionEdit from "@/components/modal/informationModal/informationDescriptionEdit";
 
 const InformationModalHead = () => {
+    const contentType = useSelector(selectModalContentType)
     const currentTimeline = useSelector(selectCurrentTimeline)
 
     return (
         <div className={'z-10'}>
             <h1 className={'timelineInformationName text-2xl font-bold'}>{currentTimeline.name}</h1>
-            <div className={'text-md'}>{currentTimeline.description}</div>
+            <div className={'relative'}>
+                {contentType === 'edit' &&  <InformationDescriptionEdit />}
+                <div className={`${contentType === 'edit' && 'invisible'} text-md`}>{currentTimeline.description}</div>
+            </div>
             <div className={'my-1 flex gap-1 text-gray-400 text-sm'}>Last Updated: January 14, 2024</div>
             <ModalMenubar/>
         </div>
