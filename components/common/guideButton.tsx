d70faa427065db58bc1a4fc072d25cf840139d6c
Nam Joon <jungeol66104@gmail.com>
2024-08-09 09:12:17 +0900

    mtest

--- components/common/guideButton.tsx
+++ components/common/guideButton.tsx
@@ -13,61 +13,93 @@ const GuideButton = ({type}: {type: string}) => {
                 <div className={'material-symbols-outlined text-[12px]'}>&#xe887;</div>
                 <span className={'mt-[1px] text-[10px]'}>Guide</span>
             </button>
-            {!isPopup &&
+            {isPopup &&
                 <Popup title={'Guide'}>
                     <div className={'flex flex-col gap-5 font-medium'}>
                         <h1 className={'text-2xl font-bold'}>Date Format</h1>
                         <p>
-                            Only YYYY-MM-DD HH:MM:SS BCE format is accepted in Timeline.
-                        </p>
-                        <p>
-                            YYYY refers to years except 0.
-                            MM refers to months from 1 to 12.
-                            DD refers to dates from 1 to 31.
-                            HH refers to hours from 0 to 23.
-                            MM and SS refers to minutes and seconds from 0 to 59.
-                            BCE is optional.
-                        </p>
-                        <p>
-                            Table below shows some examples of allowed formats.
+                            Table below shows some examples of accepted formats.
                         </p>
                         <div className="overflow-hidden text-sm font-normal border-[1px] border-gray-300 rounded-2xl bg-white">
                             <table className="w-full border-collapse rounded-2xl">
                                 <thead className={'rounded-2xl'}>
-                                    <tr className="bg-gray-100 rounded-t-2xl">
-                                        <th className="py-2 border-b-[1px] border-r-[1px] border-gray-300">Example</th>
-                                        <th className="py-2 border-b-[1px] border-gray-300">Acceptability</th>
-                                    </tr>
+                                <tr className="bg-gray-100 rounded-t-2xl">
+                                    <th className="py-2 border-b-[1px] border-r-[1px] border-gray-300">Example</th>
+                                    <th className="py-2 border-b-[1px] border-gray-300">Acceptability</th>
+                                </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23:15:00 BCE</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23:15</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 BCE</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583</td>
-                                        <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="py-2 px-4 border-r-[1px] border-gray-300">583-06 23:15:00</td>
-                                        <td className="py-2 px-4 text-center">Not Accepted</td>
-                                    </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23:15:00 BCE</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23:15</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 23</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06-23 BCE</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583-06</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">583</td>
+                                    <td className="py-2 px-4 text-center border-b-[1px] border-gray-300">Accepted</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-r-[1px] border-gray-300">583-06 23:15:00</td>
+                                    <td className="py-2 px-4 text-center">Not Accepted</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <p>
+                            Only <span className={'text-sm font-normal'}>YYYY-MM-DD HH:MM:SS BCE</span> format is accepted in Timeline.
+                        </p>
+                        <div className="overflow-hidden text-sm font-normal border-[1px] border-gray-300 rounded-2xl bg-white">
+                            <table className="w-full border-collapse rounded-2xl">
+                                <thead className={'rounded-2xl'}>
+                                <tr className="bg-gray-100 rounded-t-2xl">
+                                    <th className="py-2 border-b-[1px] border-r-[1px] border-gray-300">Element</th>
+                                    <th className="py-2 border-b-[1px] border-gray-300">Description</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">YYYY</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Years. Any number except 0.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">MM(Date)</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Months. Any number from 1 to 12.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">DD</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Days. Any number from 1 to 31.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">HH</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Hours. Any number from 0 to 23. 0 to 11 for AM, 12 to 23 for PM.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">MM(Time)</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Minutes. Any number from 0 to 59.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-gray-300">SS</td>
+                                    <td className="py-2 px-4 border-b-[1px] border-gray-300">Seconds. Any number from 0 to 59.</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2 px-4 border-r-[1px] border-gray-300">BCE</td>
+                                    <td className="py-2 px-4">Era. Without BCE, the date is referred as CE automatically.</td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
--- components/layout/popup.tsx
+++ components/layout/popup.tsx
@@ -13,7 +13,7 @@ const Popup = ({title, children}: {title: string, children: React.ReactNode}) =>
     return ReactDOM.createPortal(
         <div className={'fixed top-0 left-0 w-full h-full'}>
             <div onClick={() => dispatch(updateIsPopup(false))} className={`fixed opacity-30 w-full h-full top-0 left-0 bg-black`}></div>
-            <div className={'absolute overflow-y-hidden min-[416px]:w-[400px] max-[416px]:w-[calc(100%-16px)] min-[416px]:h-[400px] max-[416px]:h-[50%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl flex flex-col items-center'} style={{zIndex: 5003}}>
+            <div className={'absolute overflow-y-hidden min-[416px]:w-[400px] max-[416px]:w-[calc(100%-16px)] h-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl flex flex-col items-center'} style={{zIndex: 5003}}>
                 <div className={'relative py-2.5 w-full border-b-[1px] text-center'}>
                     <h2 className={'text-md font-semibold'}>{title}</h2>
                     <ClosePopupButton/>
