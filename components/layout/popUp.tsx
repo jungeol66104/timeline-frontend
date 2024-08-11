6dd4983b71c291152630d598a86c3b26ebb090e3
Nam Joon <jungeol66104@gmail.com>
2024-08-11 16:02:43 +0900

    test

--- components/layout/popUp.tsx
+++ components/layout/popUp.tsx
@@ -13,7 +13,7 @@ const PopUp = ({title, children}: {title: string, children: React.ReactNode}) =>
     return ReactDOM.createPortal(
         <div className={'fixed top-0 left-0 w-full h-full'} style={{zIndex: 5003}}>
             <div onClick={() => dispatch(updateIsPopup(false))} className={`fixed opacity-30 w-full h-full top-0 left-0 bg-black`}></div>
-            <div className={'absolute overflow-y-hidden min-[416px]:w-[400px] max-[416px]:w-[calc(100%-16px)] max-h-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl flex flex-col items-center'}>
+            <div className={'absolute overflow-y-hidden min-[432px]:w-[400px] max-[432px]:w-[calc(100%-32px)] max-h-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl flex flex-col items-center'}>
                 <div className={'relative py-2.5 w-full border-b-[1px] text-center'}>
                     <h2 className={'text-md font-semibold'}>{title}</h2>
                     <ClosePopupButton/>
