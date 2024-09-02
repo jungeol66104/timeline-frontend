939ab8805f0de74bc652957900f9615b9c17016a
Nam Joon <jungeol66104@gmail.com>
2024-09-02 14:23:59 +0900

    image efficiency

--- components/layout/popups/share/linkCopyButton.tsx
+++ components/layout/popups/share/linkCopyButton.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ShareButtonTemplate from "@/components/layout/popups/share/shareElementButtonTemplate";
 import {useDispatch} from "react-redux";
+import {updatePopupType} from "@/store/slices/appearanceSlice";
 
 const LinkCopyButton = () => {
     const dispatch = useDispatch()
     const handleClick = async () => {
         try {
             await navigator.clipboard.writeText(window.location.href);
-            dispatch(updateIsShare())
+            dispatch(updatePopupType('none'))
         } catch (error) {
             console.error('Copy failed', error);
         }
