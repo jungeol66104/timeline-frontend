7efc4ce9c4972bc11ffffec24bc39e2c62c91536
Nam Joon <jungeol66104@gmail.com>
2024-09-11 16:18:51 +0900

    add event blocked for the loginers

--- components/timelines/events/addEventButton.tsx
+++ components/timelines/events/addEventButton.tsx
@@ -1,13 +1,17 @@
 import {useDispatch, useSelector} from "react-redux";
 import {selectDemoKeyConcept, selectTimelineType, updateEventContentType, updateModalType} from "@/store/slices/appearanceSlice";
 import {selectCurrentEvents, updateCurrentEvent, updateCurrentEventDraft} from "@/store/slices/contentsSlice";
-import {getTodayDate} from "@/utils/global";
+import {getSession, getTodayDate} from "@/utils/global";
+import {selectSession, updateSession} from "@/store/slices/privateSlice";
 
 const AddEventButton = () => {
     const dispatch = useDispatch()
+    const session = useSelector(selectSession)
     const timelineType = useSelector(selectTimelineType)
     const demoKeyConcept = useSelector(selectDemoKeyConcept)
     const currentEvents = useSelector(selectCurrentEvents)
+
+    const isSession = Object.keys(session).length !== 0
 
     const handleClick = () => {
         const getNewId = () => {
@@ -15,12 +19,29 @@ const AddEventButton = () => {
             const mostNegativeId = Math.min(...negativeIds)
             return mostNegativeId === Infinity ? -1 : mostNegativeId - 1
         }
-
         let newEvent = {id: getNewId(), title: '', content: '', date: '', ephemerisTime: 0, isKeynote: 1, timelineInfo: [], createdDt: getTodayDate(), imagePath: "base-image.png", cdnUrl: "https://cdn.timeline.vg/", contributors: {counts: 1, userId: 0, username: 'you', imagePath: "base-image.png", cdnUrl: "https://cdn.timeline.vg/"}}
-        dispatch(updateCurrentEvent(newEvent))
-        dispatch(updateCurrentEventDraft(newEvent))
-        dispatch(updateModalType('event'))
-        dispatch(updateEventContentType('new'))
+
+        if (isSession) {
+            dispatch(updateCurrentEvent(newEvent))
+            dispatch(updateCurrentEventDraft(newEvent))
+            dispatch(updateModalType('event'))
+            dispatch(updateEventContentType('new'))
+        } else {
+            window.open(`/api/user/signin`, 'google-login-popup', `width=488, height=${window.screen.height}, top=0, left=${window.screen.width/2 - 244}, scrollbars=yes`);
+
+            window.addEventListener('message', (event) => {
+                if (event.origin !== window.location.origin) return;
+                if (event.data.type === 'SIGNIN_SUCCESS') {
+                    getSession().then((session) => {
+                        dispatch(updateSession(session));
+                        dispatch(updateCurrentEvent(newEvent))
+                        dispatch(updateCurrentEventDraft(newEvent))
+                        dispatch(updateModalType('event'))
+                        dispatch(updateEventContentType('new'))
+                    })
+                }
+            });
+        }
     }
 
     return (
