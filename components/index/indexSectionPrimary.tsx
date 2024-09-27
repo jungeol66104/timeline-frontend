89e5866d7dcdef4d3b7bf0731631be0f5b36991d
Nam Joon <jungeol66104@gmail.com>
2024-09-27 20:18:42 +0900

    BTS to the front

--- components/index/indexSectionPrimary.tsx
+++ components/index/indexSectionPrimary.tsx
@@ -3,13 +3,24 @@ import {useSelector} from "react-redux";
 import {selectCurrentTimelines} from "@/store/slices/contentsSlice";
 import TimelinePreview from "@/components/index/timelinePreview";
 import IndexBottom from "@/components/index/indexBottom";
+import {selectTagNum} from "@/store/slices/appearanceSlice";
 
 const IndexSectionPrimary = () => {
     const currentTimelines = useSelector(selectCurrentTimelines)
+    const tagNum = useSelector(selectTagNum)
+
+    let timelines = [...currentTimelines]
+    if (tagNum === 4) {
+        timelines.sort((a, b) => {
+            if (a.title === 'BTS') return -1;
+            if (b.title === 'BTS') return 1;
+            return 0;
+        });
+    }
 
     return (
         <div className={'relative px-4 pt-1 pb-0 h-fit w-full max-w-[630px] min-[852px]:min-w-[500px]'}>
-            {currentTimelines.map(timeline => {
+            {timelines.map(timeline => {
                 return <TimelinePreview key={timeline.id} timeline={timeline} />
             })}
             <IndexBottom/>
