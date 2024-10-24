83aed561bfcb78f1754d14fffc1698a3b674aa11
Nam Joon <jungeol66104@gmail.com>
2024-10-24 17:22:13 +0900

    toolbar size adjustment

--- components/modals/informationModal/informationEdit/newInformationEditMenubar.tsx
+++ components/modals/informationModal/informationEdit/newInformationEditMenubar.tsx
@@ -3,16 +3,16 @@ import SaveInformationButton from "@/components/modals/informationModal/informat
 
 const NewInformationEditMenubar = () => {
     return (
-        <div className={'sticky bottom-3 w-full h-[40px] flex items-center justify-between'}>
-            <div className={'p-0.5 flex items-center gap-0.5 h-[40px] border-[0.1px] border-gray-300 bg-white drop-shadow-sm rounded-md'}>
-                <button className={`material-symbols-outlined text-[20px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xe43e;</button>
-                <button className={`material-symbols-outlined text-[20px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xf85a;</button>
+        <div className={'sticky bottom-3 w-full h-[36px] flex items-center justify-between'}>
+            <div className={'p-0.5 flex items-center gap-0.5 h-[36px] border-[0.1px] border-gray-300 bg-white drop-shadow-sm rounded-md'}>
+                <button className={`material-symbols-outlined text-[20px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xe43e;</button>
+                <button className={`material-symbols-outlined text-[20px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xf85a;</button>
 
-                <button className={`material-symbols-outlined text-[25px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xf018;</button>
-                <button className={`material-symbols-outlined text-[20px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xf191;</button>
-                <button className={`material-symbols-outlined text-[20px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xe241;</button>
+                <button className={`material-symbols-outlined text-[25px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xf018;</button>
+                <button className={`material-symbols-outlined text-[20px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xf191;</button>
+                <button className={`material-symbols-outlined text-[20px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xe241;</button>
 
-                <button className={`material-symbols-outlined text-[22px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xe178;</button>
+                <button className={`material-symbols-outlined text-[22px] w-9 h-8 rounded-md hover:bg-gray-100`}>&#xe178;</button>
                 {/*<button className={`material-symbols-outlined text-[22px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xe238;</button>*/}
                 {/*<button className={`material-symbols-outlined text-[20px] w-9 h-9 rounded-md hover:bg-gray-100`}>&#xe257;</button>*/}
             </div>
