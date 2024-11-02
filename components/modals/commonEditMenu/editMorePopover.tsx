a47d585fe0c73276c2e0723af8c6e4fa75d8ac97
Nam Joon <jungeol66104@gmail.com>
2024-11-02 15:16:03 +0900

    editMoreButton layout

--- components/modals/commonEditMenu/editMorePopover.tsx
+++ components/modals/commonEditMenu/editMorePopover.tsx
@@ -20,18 +20,18 @@ const EditMorePopover = () => {
         <div id={'editMorePopover'}
              className={`${editPopoverType !== 'editMore' && 'hidden'} absolute bottom-0 p-1.5 pt-2 w-full max-w-[300px] max-h-[170px] flex flex-col gap-1.5 border-[0.1px] border-gray-300 bg-white drop-shadow-sm rounded-md`}
              style={{right: 0}}>
-            <div className={'flex items-center gap-2'}>
-                <div className={'pl-1.5 text-sm line-clamp-2'}>Select a thumbnail link displayed in timeline</div>
+            <div className={'flex items-center justify-between gap-2'}>
+                <div className={'pl-1.5 text-sm font-medium line-clamp-2'}>Select a thumbnail link</div>
                 <button className={`shrink-0 px-2.5 w-[100px] h-9 text-sm font-semibold hover:bg-gray-100 border-[0.1px] border-gray-300 drop-shadow-sm rounded-md`}>Links</button>
             </div>
             <hr/>
-            <div className={'flex items-center gap-2'}>
-                <div className={'pl-1.5 text-sm line-clamp-2'}>Check for showing the event in entry view</div>
+            <div className={'flex items-center justify-between gap-2'}>
+                <div className={'pl-1.5 text-sm font-medium line-clamp-2'}>Show in entry view</div>
                 <KeynoteButton/>
             </div>
             <hr/>
             <div className={'flex items-center justify-between gap-2'}>
-                <div className={'pl-1.5 text-sm line-clamp-2'}>Remove the event from the timeline</div>
+                <div className={'pl-1.5 text-sm font-medium line-clamp-2'}>Remove from the timeline</div>
                 <DetachButton/>
             </div>
             {(modalType === 'event' && (contentType === 'edit' || (contentType === 'new' && isCreated))) &&
