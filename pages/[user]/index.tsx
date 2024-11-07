e97bb8f693d9de167a041445e4af2f6031e252e5
Nam Joon <jungeol66104@gmail.com>
2024-11-07 12:05:32 +0900

    vercel issue

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -11,8 +11,8 @@ import useOperateProfile from "@/hooks/useOperateProfile";
 import axios from "axios";
 
 export const getServerSideProps = storeWrapper.getServerSideProps((store) => async ({params, req}) => {
-    const protocol = process.env.VERCEL_ENV ? 'https' : (req.headers['x-forwarded-proto'] || 'http');
-    const host = req.headers.host;
+    const protocol = req.headers['x-forwarded-proto'] === 'https' ? 'https' : 'http';
+    const host = req.headers.host.split('/')[0];
     const baseUrl = `${protocol}://${host}`;
 
     const user = params?.user as string
